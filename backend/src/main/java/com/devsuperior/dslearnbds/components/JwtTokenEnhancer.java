4002b005e4afd1d86c5d2ffffa0172e3cbd10d33
Andre Silveira <alasandre@gmail.com>
2021-12-19 17:51:50 -0300

    Refresh token

--- backend/src/main/java/com/devsuperior/dslearnbds/components/JwtTokenEnhancer.java
+++ backend/src/main/java/com/devsuperior/dslearnbds/components/JwtTokenEnhancer.java
@@ -23,6 +23,7 @@ public class JwtTokenEnhancer implements TokenEnhancer {
 	public OAuth2AccessToken enhance(OAuth2AccessToken accessToken, OAuth2Authentication authentication) {
 		User user = userRepository.findByEmail(authentication.getName());
 		Map<String, Object> map = new HashMap<>();
+		map.put("userName", user.getName());
 		map.put("userId", user.getId());
 		
 		DefaultOAuth2AccessToken token = (DefaultOAuth2AccessToken) accessToken;
